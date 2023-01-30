9264db5a6c32ea2b98314a67e8aaf64e282c4ae9
jiyoonpark <jiyoonp1992@gmail.com>
2023-01-30 18:00:15 +0900

    '0130'

---
+++ src/Level0/level0_78_2.java
@@ -0,0 +1,45 @@
+package Level0;
+
+import java.util.Arrays;
+
+public class level0_78_2 {
+	//
+	public static void main(String[] args) {
+		System.out.println(solution("1 2 Z 3"));
+		System.out.println(solution("10 20 30 40"));
+		System.out.println(solution("10 Z 20 Z 1"));
+		System.out.println(solution("10 Z 20 Z -1"));
+		System.out.println(solution("10 Z 20 Z"));
+		System.out.println(solution("-1 -2 -3 Z"));
+	}
+	
+	public static int solution(String s) {
+        int answer = 0;
+		boolean check = false;
+		check = 1 <= s.length() && s.length() <= 200;
+		if(check) {
+			int num = 0, idx = 0;
+			for (int i = 0; i < s.length();) {
+				idx = s.indexOf(' ');
+				if(idx != -1) {
+					if(s.charAt(0) == 'Z') {
+						answer -= num;
+					} else {
+						num = Integer.parseInt(s.substring(0,idx));
+						answer += num;
+					}
+					s = s.substring(idx).trim();
+				} else {
+					if(s.charAt(0) == 'Z') {
+						answer -= num;
+					} else {
+						num = Integer.parseInt(s);
+						answer += num;
+					}
+					s = "";
+				}
+			}
+		}
+        return answer;
+	}
+}
---
+++ src/Level0/level0_80.java
@@ -0,0 +1,19 @@
+package Level0;
+
+import java.util.Arrays;
+
+public class level0_80 {
+	//외계어 사전
+	public static void main(String[] args) {
+		System.out.println(solution());
+	}
+	
+	public static int solution(String[] spell, String[] dic) {
+        int answer = 0;
+		boolean check = false;
+		check = true;
+		if(check) {
+		}
+        return answer;
+	}
+}
