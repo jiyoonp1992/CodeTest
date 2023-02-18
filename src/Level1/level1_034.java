6a0b688772dff2eebe10062a1205f237e1519ef6
Park Jiyoon <jiyoonp92@naver.com>
2023-02-18 23:43:54 +0900

    0218

--- src/Level1/level1_034.java
+++ src/Level1/level1_034.java
@@ -1,20 +1,36 @@
 package Level1;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 import java.util.*;
 
 public class level1_034 {
 	//이상한 문자 만들기
 	public static void main(String[] args) {
-		//System.out.println(solution());
+		//System.out.println(solution("try   hello world "));
+		//System.out.println(solution("try   hello world   "));
+		System.out.println(solution("try   hello    world   "));
+		//System.out.println(solution("try   hello world"));
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
-	public static int solution() {
-        int answer = 0;
-		boolean check = false;
-		if(check) {
-		}
+	public static String solution(String s) {
+        String answer = "";
+        char[] strArr = s.toCharArray();
+        int cnt = 0;
+        for(char arr : strArr) {
+        	if(arr == ' ') {
+        		cnt = 0;
+        	} else {
+        		if(cnt % 2 == 0) {
+        			arr = Character.toUpperCase(arr);
+        		} else {
+        			arr = Character.toLowerCase(arr);
+        		}
+        		cnt++;
+        	}
+        	answer += arr;
+        }
         return answer;
 	}
 }
