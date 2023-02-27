b59403380ff871f5fd5a29878e3bb8f1e1e7268c
jiyoonpark <jiyoonp1992@gmail.com>
2023-02-27 17:31:54 +0900

    0227

--- src/Level1/level1_053.java
+++ src/Level1/level1_053.java
@@ -1,50 +1,90 @@
 package Level1;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.*;
 
 public class level1_053 {
 	//실패율
 	public static void main(String[] args) {
+		//int[] num01 = {2, 1, 2, 6, 2, 4, 3, 3};
+		//int[] num01 = {3,3,3,3,3};
 		int[] num01 = {2, 1, 2, 6, 2, 4, 3, 3};
 		//System.out.println(solution());
 		System.out.println(Arrays.toString(solution(5, num01)));
 	}
 	
+//	public static int[] solution(int N, int[] stages) {
+//		int[] answer = {};
+//	    double[] failureRates = new double[N];
+//
+//	    int numOfPlayers = stages.length;
+//
+//	    for (int i = 1; i <= N; i++) {
+//	        int numOfUnsolvedPlayers = 0;
+//	        for (int j = 0; j < stages.length; j++) {
+//	            if (stages[j] == i) {
+//	                numOfUnsolvedPlayers++;
+//	            }
+//	        }
+//
+//	        if (numOfUnsolvedPlayers == 0) {
+//	            failureRates[i - 1] = 0;
+//	        } else {
+//	            failureRates[i - 1] = (double) numOfUnsolvedPlayers / numOfPlayers;
+//	        }
+//
+//	        numOfPlayers -= numOfUnsolvedPlayers;
+//	    }
+//
+//	    // failureRates를 내림차순으로 정렬하여 answer 배열에 넣어줌
+//	    answer = IntStream.range(0, N)
+//	                      .boxed()
+//	                      .sorted((i, j) -> Double.compare(failureRates[j], failureRates[i]))
+//	                      .mapToInt(i -> i + 1)
+//	                      .toArray();
+//
+//	    return answer;
+//	}
+	
+	
 	public static int[] solution(int N, int[] stages) {
-        int[] answer = {};
-        int[] chAnswer = {};
-		boolean check = 1 <= N && N <= 500 && 1 <= stages.length && stages.length <= 200000;
-		if(check) {
-			chAnswer = new int [N];
-			answer = new int[N];
-			for(int i : stages) {
-				if(i < N) {
+		int[] answer = {};
+		double[] chAnswer = {};
+		
+		chAnswer = new double[N];
+		answer = new int[N];
+		for(int i : stages) {
+			if(i <= N) {
 					chAnswer[i-1]++;
 				}
-			}
-			int x = stages.length, cnt = 0;
-			for(int j : chAnswer) {
-				chAnswer[cnt] = j * 100 / x;
-				x -= j;
-				cnt++;
-			}
-			answer = Arrays.copyOf(chAnswer, chAnswer.length);
-			
-			Integer[] arr = Arrays.stream(chAnswer).boxed().toArray(Integer[]::new);
-			
-			Arrays.sort(arr, Collections.reverseOrder());
-			
-			cnt = 0;
-			for(int k : arr) {
-				int index = Arrays.binarySearch(chAnswer, k);
-				System.out.println(index);
-				chAnswer[index - 1] = -1;
-				answer[cnt] = index;
-				cnt++;
-			}
-			
 		}
-        return answer;
+		
+		int x = stages.length, y = x;
+		
+		for(int i = 0; i < N; i++) {
+			y -= chAnswer[i];
+			if(chAnswer[i] != 0) {
+				chAnswer[i] /= x;
+			} else {
+				chAnswer[i] = 0;
+			}
+			x = y;
+		}
+		
+		int cnt = 0;
+		
+		String[] arr =  Arrays.stream(chAnswer).mapToObj(String::valueOf).toArray(String[]::new);
+		Arrays.sort(arr, Collections.reverseOrder());
+		for(String k : arr) {
+			String[] strArray = Arrays.stream(chAnswer).mapToObj(String::valueOf).toArray(String[]::new);
+			
+			
+			int index = Arrays.asList(strArray).indexOf(k);
+			chAnswer[index] = -1;
+			answer[cnt] = index + 1;
+			cnt++;
+		}
+		return answer;
 	}
 }
---
+++ src/Level1/level1_054.java
@@ -0,0 +1,20 @@
+package Level1;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class level1_054 {
+	//[1차] 다트 게임
+	public static void main(String[] args) {
+		//System.out.println(solution());
+		//System.out.println(Arrays.toString(solution()));
+	}
+	
+	public static int solution(String dartResult) {
+        int answer = 0;
+		boolean check = false;
+		if(check) {
+		}
+        return answer;
+	}
+}
