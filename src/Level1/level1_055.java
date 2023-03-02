10a77cc94150a7138f657ed357f57474efc6d39d
Park jiYoon <jiyoonp1992@gmail.com>
2023-03-02 23:07:04 +0900

    0302

--- src/Level1/level1_055.java
+++ src/Level1/level1_055.java
@@ -6,7 +6,10 @@ import java.util.*;
 public class level1_055 {
 	//과일 장수
 	public static void main(String[] args) {
-		//System.out.println(solution());
+		int[] num01 = {1, 2, 3, 1, 2, 3, 1};
+		int[] num02 = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+		System.out.println(solution(3,4,num01));
+		System.out.println(solution(4,3,num02));
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
@@ -15,11 +18,24 @@ public class level1_055 {
 		boolean check = 3 <= k && k <= 9 && 3 <= m && m <= 10 && 7 <= score.length && score.length <= 1000000;
 		if(check) {
 			Arrays.sort(score);
-			int[] arr = {0, 0, 0};
-			while(score.length < 3) {
-				
+			int min = 10;
+			for(int i = 0; i < score.length; i++) {
+				min = Math.min(min, score[score.length - 1 - i]);
+				if((i + 1) % m == 0) {
+					answer += min * m;
+					min = 10;
+				}
 			}
 		}
         return answer;
 	}
 }
+
+//for(int i = 0; i < score.length; i++) {
+//	min = 10;
+//	for(int i = 0; i < m; i++) {
+//		min = Math.min(min, score[score.length - 1 - i]);
+//	}
+//	score = Arrays.copyOfRange(score, 0, score.length - m);
+//	answer += min * m;
+//}
---
+++ src/Level1/level1_056.java
@@ -0,0 +1,20 @@
+package Level1;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class level1_056 {
+	//명예의 전당 (1)
+	public static void main(String[] args) {
+		//System.out.println(solution());
+		//System.out.println(Arrays.toString(solution()));
+	}
+	
+	public static int[] solution(int k, int[] score) {
+        int[] answer = {};
+		boolean check = 3 <= k && k <= 100 && 7 <= score.length && score.length <= 1000;
+		if(check) {
+		}
+        return answer;
+	}
+}
