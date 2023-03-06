92b3953da44cd008fc702d9b1ef01df7e030066b
jiyoonpark <jiyoonp1992@gmail.com>
2023-03-06 17:30:57 +0900

    0306

--- src/Level1/level1_056.java
+++ src/Level1/level1_056.java
@@ -9,8 +9,8 @@ public class level1_056 {
 		int[] num01 = {10, 100, 20, 150, 1, 100, 200};
 		int[] num02 = {10, 30, 40, 3, 0, 20, 4};
 		//System.out.println(solution(3, num01));
-		//System.out.println(Arrays.toString(solution(100, num01)));
-		System.out.println(Arrays.toString(solution(9, num02)));
+		System.out.println(Arrays.toString(solution(3, num01)));
+		//System.out.println(Arrays.toString(solution(9, num02)));
 		//10, 10, 10, 3, 0, 0, 0
 	}
 	
@@ -19,24 +19,23 @@ public class level1_056 {
 		boolean check = 3 <= k && k <= 100 && 7 <= score.length && score.length <= 1000;
 		if(check) {
 			answer = new int[score.length];
-			int[] numArr = new int[k];
+			int[] numArr = {};
 			int cnt = 0;
 			for(int num : score) {
-				if(cnt == 0) {
-					Arrays.fill(numArr, num);
+				if(cnt < k) {
+					numArr = Arrays.copyOf(numArr, numArr.length + 1);
+					Arrays.sort(numArr);
+					numArr[0] = num;
 				} else {
+					if(numArr.length != k) {
+						numArr = Arrays.copyOf(numArr, k);
+					}
+					
 					if(num > numArr[0]) {
 						numArr[0] = num;
-					} else {
-						if(cnt < score.length) {
-							numArr[0] = num;
-						}
 					}
 				}
-				System.out.println(num);
-				System.out.println(cnt);
 				Arrays.sort(numArr);
-				System.out.println(Arrays.toString(numArr));
 				answer[cnt] = numArr[0];
 				cnt++;
 			}
---
+++ src/Level1/level1_057.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class level1_057 {
+	//로또의 최고 순위와 최저 순위
+	public static void main(String[] args) {
+		//System.out.println(solution());
+		//System.out.println(Arrays.toString(solution()));
+	}
+	
+	public static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+		boolean check = false;
+		Arrays.asList(lottos).contains(win_nums);
+        return answer;
+	}
+}
