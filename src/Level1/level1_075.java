0597e1fbf57699f3f49777eccf379ce6f8e82f90
Park Jiyoon <jiyoonp92@naver.com>
2023-03-18 23:49:35 +0900

    0318

---
+++ src/Level1/level1_075.java
@@ -0,0 +1,23 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class level1_075 {
+	//당구 연습
+	public static void main(String[] args) {
+		int[][] num01 = {{7, 7}, {2, 7}, {7, 3}};
+		//System.out.println(solution());
+		System.out.println(Arrays.toString(solution(10, 10, 3, 7, num01)));
+	}
+	
+	public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        answer = new int[balls.length];
+        int x = 0, y = 0;
+        for(int i = 0; i < answer.length; i++) {
+        	// x y 기준잡고 가까운쪽으로 변호나
+
+        }
+        return answer;
+	}
+}
