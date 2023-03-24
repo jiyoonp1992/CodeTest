8debdfb6b830d4bdaaaf35ce537c734c15468164
jiyoonpark <jiyoonp1992@gmail.com>
2023-03-24 17:51:18 +0900

    0324

--- src/Level2/level2_015.java
+++ src/Level2/level2_015.java
@@ -5,12 +5,38 @@ import java.util.Arrays;
 public class level2_015 {
 	//N개의 최소공배수
 	public static void main(String[] args) {
-		//System.out.println(solution());
+		int[] num01 = {10,12};
+		System.out.println(solution(num01));
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
 	public static int solution(int[] arr) {
         int answer = 0;
+        Arrays.sort(arr);
+        int x = 0, y = 0, z = 0, min = 0, max = 0;
+        for(int i = 1; i < arr.length; i++) {
+        	if(i == 1) {
+        		x = arr[0];
+        		y = arr[i];
+        	} else {
+        		x = Math.min(x, arr[i]);
+        		y = Math.max(x, arr[i]);
+        	}
+        	
+        	if(y%x == 0) {
+        		z = x;
+        	} else {
+        		min = x;
+        		max = y;
+        		for(;max%min != 0;) {
+        			z = max % min;
+        			
+        		}
+        	}
+        }
+        
+        answer = (x * y) / x;
+        
         return answer;
 	}
 }
