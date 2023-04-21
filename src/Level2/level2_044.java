c190f3274d2928dcc747bb8732bf1d4b50799151
jiyoonpark <jiyoonp1992@gmail.com>
2023-04-21 17:59:14 +0900

    0421

---
+++ src/Level2/level2_044.java
@@ -0,0 +1,68 @@
+package Level2;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import javax.xml.soap.Node;
+
+public class level2_044 {
+	//게임 맵 최단거리
+	public static void main(String[] args) {
+		int[][] num = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+		
+		System.out.println(solution(num));
+		//System.out.println(Arrays.toString(solution()));
+	}
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { -1, 0, 1, 0 };
+	static boolean[][] visited;
+	static int n, m;
+	int dfs_answer = -1;
+	
+	public static int solution(int[][] maps) {
+        int answer = 0;
+        n = maps.length;
+		m = maps[0].length;
+
+		visited = new boolean[n][m];
+		System.out.println("bfs : " + bfs(0, 0, maps));
+        return answer;
+	}
+	public static class Node {
+		int x;
+		int y;
+		int cost;
+
+		public Node(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+		}
+	}
+	public static int bfs(int x, int y, int[][] maps) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(x, y, 1));
+		visited[x][y] = true;
+
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			if (node.x == n - 1 && node.y == m - 1)
+				return node.cost;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = node.x + dx[i];
+				int ny = node.y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+					if (maps[nx][ny] == 1 && !visited[nx][ny]) {
+						visited[nx][ny] = true;
+						q.offer(new Node(nx, ny, node.cost + 1));
+					}
+				}
+			}
+		}
+		return -1;
+	}
+}
