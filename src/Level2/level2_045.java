c62a83db4613e11f4fe13dde1ac9a163d6a3f6b2
Park Jiyoon <jiyoonp92@naver.com>
2023-04-25 21:38:59 +0900

    0425

--- src/Level2/level2_045.java
+++ src/Level2/level2_045.java
@@ -1,6 +1,8 @@
 package Level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class level2_045 {
 	//모음 사전
@@ -9,36 +11,60 @@ public class level2_045 {
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
-	static boolean[] visit = new boolean[5];
+//	static boolean[] visit = new boolean[5];
+//	public static int solution(String word) {
+//        int answer = 0;
+//        char[] chArr = {'A', 'E', 'I', 'O', 'U'};
+////        for(;;) {
+////        	
+////        }
+//        answer = dfs(0,word, chArr,0);
+//        return answer;
+//	}
+//	
+//	public static int dfs(int x, String word, char[] chArr, int depth) {
+//		StringBuilder sb = new StringBuilder();
+//		if(sb.equals(word)) {
+//			return x;
+//		}
+//		
+//			if (!visit[depth] && depth < chArr.length) {
+//				sb.append(chArr[depth]);
+//				visit[depth] = true;
+//				System.out.println(Arrays.toString(visit));
+//				System.out.println(sb.toString());
+//				System.out.println("--------------------");
+//				dfs(depth, word, chArr, depth + 1);
+//				visit[depth] = false;
+//				sb.deleteCharAt(sb.length() - 1);
+//				System.out.println(Arrays.toString(visit));
+//				System.out.println(sb.toString());
+//				System.out.println("++++++++++++++++++++");
+//			}
+//		return x;
+//	}
+	static String[] arr;
+    static List<String> list;
 	public static int solution(String word) {
         int answer = 0;
-        char[] chArr = {'A', 'E', 'I', 'O', 'U'};
-//        for(;;) {
-//        	
-//        }
-        answer = dfs(0,word, chArr,0);
+        
+        list = new ArrayList<>();
+        arr = new String[]{"A", "E", "I", "O", "U"};
+        dictionList(word, "", 0);
+        
+        answer = list.indexOf(word);
         return answer;
 	}
 	
-	public static int dfs(int x, String word, char[] chArr, int depth) {
-		StringBuilder sb = new StringBuilder();
-		if(sb.equals(word)) {
-			return x;
-		}
-		
-			if (!visit[depth] && depth < chArr.length) {
-				sb.append(chArr[depth]);
-				visit[depth] = true;
-				System.out.println(Arrays.toString(visit));
-				System.out.println(sb.toString());
-				System.out.println("--------------------");
-				dfs(depth, word, chArr, depth + 1);
-				visit[depth] = false;
-				sb.deleteCharAt(sb.length() - 1);
-				System.out.println(Arrays.toString(visit));
-				System.out.println(sb.toString());
-				System.out.println("++++++++++++++++++++");
-			}
-		return x;
-	}
+    static void dictionList(String word, String str, int depth) {
+        list.add(str);
+        
+        if(depth == 5) {
+            return;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+        	dictionList(word, str + arr[i], depth + 1);
+        }
+    }
 }
