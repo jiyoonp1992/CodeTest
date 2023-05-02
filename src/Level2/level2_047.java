bd6a927d28308228fd65b2c8276ef355b6656f0f
Park Jiyoon <jiyoonp92@naver.com>
2023-05-02 23:19:50 +0900

    0502

--- src/Level2/level2_047.java
+++ src/Level2/level2_047.java
@@ -13,30 +13,76 @@ public class level2_047 {
 	}
 	
 	public static int solution(int m, int n, String[] board) {
+//        int answer = 0;
+//        List<String> list = new ArrayList<String>();
+//        int x = 0, y = 0;
+//        //for(int k = 0; k < 1;) {
+//        	for(int i = 0; i < m - 1; i++) {
+//        		for(int j = 0; j < n - 1; j++) {
+//        			if(board[i].charAt(j) == board[i].charAt(j + 1) && board[i].charAt(j) == board[i + 1].charAt(j) && board[i].charAt(j) == board[i + 1].charAt(j + 1)) {
+//        				list.add(i + "/" + j);
+//        				answer += 4;
+//        			}
+//        			System.out.println(board[i].charAt(j));
+//        			System.out.println(answer);
+//        			System.out.println(list);
+//        		}
+//        	}
+//        	for(int o = 0; o < list.size(); o++) {
+//        		x = Integer.valueOf(list.get(o).substring(0,1)).intValue();
+//        		y = Integer.valueOf(list.get(o).substring(2,3)).intValue();
+//        		board[i].charAt(index)
+//        	}
+//        	System.out.println(x);
+//        	System.out.println(y);
+//        //}
+//        
+//        return answer;
+		
         int answer = 0;
         List<String> list = new ArrayList<String>();
         int x = 0, y = 0;
-        //for(int k = 0; k < 1;) {
-        	for(int i = 0; i < m - 1; i++) {
-        		for(int j = 0; j < n - 1; j++) {
-        			if(board[i].charAt(j) == board[i].charAt(j + 1) && board[i].charAt(j) == board[i + 1].charAt(j) && board[i].charAt(j) == board[i + 1].charAt(j + 1)) {
-        				list.add(i + "/" + j);
-        				answer += 4;
-        			}
-        			System.out.println(board[i].charAt(j));
-        			System.out.println(answer);
-        			System.out.println(list);
-        		}
+        for(int i = 0; i < m - 1; i++) {
+    		for(int j = 0; j < n - 1; j++) {
+    			if(board[i].charAt(j) == board[i].charAt(j + 1) && board[i].charAt(j) == board[i + 1].charAt(j) && board[i].charAt(j) == board[i + 1].charAt(j + 1)) {
+    				if(!list.contains(i + "/" + j)) {
+    					list.add(i + "/" + j);
+    					answer++;
+    				}
+    				if(!list.contains((i + 1) + "/" + j)) {
+    					list.add((i + 1) + "/" + j);
+    					answer++;
+    				}
+    				if(!list.contains(i + "/" + (j + 1))) {
+    					list.add(i + "/" + (j + 1));
+    					answer++;
+    				}
+    				if(!list.contains((i + 1) + "/" + (j + 1))) {
+    					list.add((i + 1) + "/" + (j + 1));
+    					answer++;
+    				}
+    			}
+    		}
+    	}
+        System.out.println(list);
+        char[] charArr = {};
+        char temp = ' ';
+        for(int o = 0; o < list.size(); o++) {
+        	x = Integer.valueOf(list.get(o).substring(0,1)).intValue();
+        	y = Integer.valueOf(list.get(o).substring(2,3)).intValue();
+        	x = x == 0 ? x : x - 1;
+        	if(o % 2 == 0) {
+        		charArr = board[x].toCharArray();
+        		temp = charArr[y];
+        		charArr[y] = '0';
+        		board[x] = new String(charArr);
+        	} else {
+        		charArr = board[x].toCharArray();
+        		charArr[y] = temp;
+        		board[x] = new String(charArr);
         	}
-        	for(int o = 0; o < list.size(); o++) {
-        		x = Integer.valueOf(list.get(o).substring(0,1)).intValue();
-        		y = Integer.valueOf(list.get(o).substring(2,3)).intValue();
-        		board[i].charAt(index)
-        	}
-        	System.out.println(x);
-        	System.out.println(y);
-        //}
-        
+    		System.out.println(Arrays.toString(charArr));
+    	}
         return answer;
 	}
 }
