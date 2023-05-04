b9729459e6e6ec0e363ad88ef3577375db867c28
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-04 15:50:44 +0900

    0504

--- src/Level2/level2_047.java
+++ src/Level2/level2_047.java
@@ -7,8 +7,10 @@ import java.util.List;
 public class level2_047 {
 	//[1차] 프렌즈4블록
 	public static void main(String[] args) {
-		String[] str = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
-		System.out.println(solution(6,6,str));
+		//String[] str = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+		String[] str = {"AA", "AA", "CC", "AA", "AA", "DD"};
+		//System.out.println(solution(5,5,str));
+		System.out.println(solution(6,2,str));
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
@@ -42,46 +44,56 @@ public class level2_047 {
         int answer = 0;
         List<String> list = new ArrayList<String>();
         int x = 0, y = 0;
-        for(int i = 0; i < m - 1; i++) {
-    		for(int j = 0; j < n - 1; j++) {
-    			if(board[i].charAt(j) == board[i].charAt(j + 1) && board[i].charAt(j) == board[i + 1].charAt(j) && board[i].charAt(j) == board[i + 1].charAt(j + 1)) {
-    				if(!list.contains(i + "/" + j)) {
-    					list.add(i + "/" + j);
-    					answer++;
-    				}
-    				if(!list.contains((i + 1) + "/" + j)) {
-    					list.add((i + 1) + "/" + j);
-    					answer++;
-    				}
-    				if(!list.contains(i + "/" + (j + 1))) {
-    					list.add(i + "/" + (j + 1));
-    					answer++;
-    				}
-    				if(!list.contains((i + 1) + "/" + (j + 1))) {
-    					list.add((i + 1) + "/" + (j + 1));
-    					answer++;
-    				}
-    			}
-    		}
-    	}
-        System.out.println(list);
-        char[] charArr = {};
-        char[] charTempArr = {};
-        char temp = ' ';
-        for(int o = n; o >= 0; o--) {
-        	x = Integer.valueOf(list.get(o).substring(0,1)).intValue();
-        	y = Integer.valueOf(list.get(o).substring(2,3)).intValue();
-        	if(x != 0) {
-        		charTempArr = board[x-1].toCharArray();
-        		charArr = board[x].toCharArray();
-        		temp = charTempArr[y];
-        		charTempArr[y] = ' ';
-        		charArr[y] = temp;
-        		board[x-1] = new String(charTempArr);
-        		board[x] = new String(charArr);
+        for(int z = 0; z < 1;) {
+        	for(int i = 0; i < m - 1; i++) {
+        		for(int j = 0; j < n - 1; j++) {
+        			if(board[i].charAt(j) != ' ' && board[i].charAt(j) == board[i].charAt(j + 1) && board[i].charAt(j) == board[i + 1].charAt(j) && board[i].charAt(j) == board[i + 1].charAt(j + 1)) {
+        				if(!list.contains(i + "/" + j)) {
+        					list.add(i + "/" + j);
+        					answer++;
+        				}
+        				if(!list.contains((i + 1) + "/" + j)) {
+        					list.add((i + 1) + "/" + j);
+        					answer++;
+        				}
+        				if(!list.contains(i + "/" + (j + 1))) {
+        					list.add(i + "/" + (j + 1));
+        					answer++;
+        				}
+        				if(!list.contains((i + 1) + "/" + (j + 1))) {
+        					list.add((i + 1) + "/" + (j + 1));
+        					answer++;
+        				}
+        			} else {
+        				z = 1;
+        			}
+        		}
         	}
-    	}
-        System.out.println(Arrays.toString(board));
+        	char[] charArr = {};
+        	char[] charTempArr = {};
+        	char temp = ' ';
+        	for(int o = 0; o < list.size(); o++) {
+        		x = Integer.valueOf(list.get(o).substring(0,1)).intValue();
+        		y = Integer.valueOf(list.get(o).substring(2,3)).intValue();
+        		for(;x > 0;x--) {
+        			if(x != 0) {
+        				charTempArr = board[x-1].toCharArray();
+        				charArr = board[x].toCharArray();
+        				temp = charTempArr[y];
+        				charTempArr[y] = ' ';
+        				charArr[y] = temp;
+        				board[x-1] = new String(charTempArr);
+        				board[x] = new String(charArr);
+        			} else {
+        				charTempArr = board[x].toCharArray();
+        				charTempArr[y] = ' ';
+        				board[x] = new String(charTempArr);
+        			}
+        		}
+        		z = 0;
+        	}
+        	list.clear();
+        }
         return answer;
 	}
 }
