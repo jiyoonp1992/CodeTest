3b867381b708d2d6295806b4ce9f510ba088236e
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-09 17:56:25 +0900

    0509

--- src/Level2/level2_048.java
+++ src/Level2/level2_048.java
@@ -1,31 +1,51 @@
 package Level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class level2_048 {
 	//뒤에 있는 큰 수 찾기
 	public static void main(String[] args) {
-		int[] num = {2, 3, 3, 5};
+		int[] num = {9, 1, 5, 3, 6, 2};
 		//System.out.println(solution());
 		System.out.println(Arrays.toString(solution(num)));
 	}
 	
 	public static int[] solution(int[] numbers) {
         int[] answer = {};
-        int[] chNum = {};
-        chNum = Arrays.copyOf(numbers, numbers.length);
+        int[] reNum = {};
+        reNum = Arrays.copyOf(numbers, numbers.length);
+        Arrays.sort(reNum);
         answer = new int[numbers.length];
-        int cnt = numbers.length - 1;
-        Arrays.sort(chNum);
-        for(int i = 0; i < numbers.length; i++) {
-        	for(int j = i; j < numbers.length; j++) {
-        		if(numbers[i] < numbers[j]) {
-        			answer[i] = numbers[j];
-        			break;
-        		} else {
+        List<Integer> list = new ArrayList<>();
+        Arrays.fill(answer, 0);
+        answer[answer.length - 1] = -1;
+        int check = 0, reCh = reNum.length - 1;
+        while(check >= 0) {
+        	for(int i = 0; i < numbers.length - 1; i++) {
+        		System.out.println(numbers[i]);
+        		System.out.println(list);
+        		if(numbers[i] == reNum[reCh]) {
         			answer[i] = -1;
+        			reCh--;
+        		} else {
+        			if(numbers[i] == numbers[i + 1]) {
+        				list.add(i);
+        			} else if (numbers[i] < numbers[i + 1]) {
+        				if(list.isEmpty()) {
+        					answer[i] = numbers[i + 1];
+        				} else {
+        					list.add(i);
+        					for(int j = 0; j < list.size();j++) {
+        						answer[list.get(j)] = numbers[i + 1];
+        					}
+        				}
+        			}
         		}
         	}
+        	check = Arrays.toString(answer).indexOf("0");
+        	System.out.println(check);
         }
         return answer;
 	}
