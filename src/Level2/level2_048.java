b6f04c9e95bfd76d35dd56257e1b8ca1a5dfe26a
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-12 17:07:48 +0900

    0512

--- src/Level2/level2_048.java
+++ src/Level2/level2_048.java
@@ -6,38 +6,58 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class level2_048 {
 	//뒤에 있는 큰 수 찾기
 	public static void main(String[] args) {
-		//int[] num = {9, 1, 5, 3, 6, 2};
-		int[] num = {8, 1, 2, 9};
+		int[] num = {9, 1, 5, 3, 6, 2};
+		//int[] num = {8, 1, 2, 9};
 		//System.out.println(solution());
 		System.out.println(Arrays.toString(solution(num)));
 	}
 	
 	public static int[] solution(int[] numbers) {
 		int[] answer = {};
-		int[] chNum = {};
-		chNum = Arrays.copyOf(numbers, numbers.length);
 		answer = new int[numbers.length];
-		answer[numbers.length - 1] = -1;
-		int cnt = numbers.length - 1;
-		Arrays.sort(chNum);
-		for(int i = 0; i < numbers.length - 1; i++) {
-			if(numbers[i] == chNum[cnt]) {
-				answer[i] = -1;
-				cnt--;
-			} else {
-				for(int j = i + 1; j < numbers.length; j++) {
-					if(numbers[i] < numbers[j]) {
-						answer[i] = numbers[j];
-						break;
+		Arrays.fill(answer, -1);
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < numbers.length - 1; i++) {
+			stack.add(i);
+			for(int j = i + 1; j < numbers.length; j++) {
+				if(numbers[i] == numbers[j]) {
+					stack.add(j);
+				} else if(numbers[i] < numbers[j]) {
+					while(!stack.isEmpty()) {
+						answer[stack.pop()] = numbers[j];
 					}
+					break;
 				}
 			}
+			stack.clear();
 		}
 		return answer;
+//		int[] answer = {};
+//		int[] chNum = {};
+//		chNum = Arrays.copyOf(numbers, numbers.length);
+//		answer = new int[numbers.length];
+//		answer[numbers.length - 1] = -1;
+//		int cnt = numbers.length - 1;
+//		Arrays.sort(chNum);
+//		for(int i = 0; i < numbers.length - 1; i++) {
+//			if(numbers[i] == chNum[cnt]) {
+//				answer[i] = -1;
+//				cnt--;
+//			} else {
+//				for(int j = i + 1; j < numbers.length; j++) {
+//					if(numbers[i] < numbers[j]) {
+//						answer[i] = numbers[j];
+//						break;
+//					}
+//				}
+//			}
+//		}
+//		return answer;
 		
 //		int[] answer = {};
 //		int[] chNum = {};
