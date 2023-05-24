898cef229eeb836c0f26309ef0043f72ef237a30
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-24 17:29:38 +0900

    0524

--- src/Level2/level2_050.java
+++ src/Level2/level2_050.java
@@ -12,30 +12,57 @@ public class level2_050 {
 	
 	public static long[] solution(long[] numbers) {
         long[] answer = {};
-        String str = "";
-        String strNext = "";
-        answer = new long[numbers.length];
-        int x = 500;
-        int y = 505;
-        str = Integer.toBinaryString(x);
-        strNext = Integer.toBinaryString(y);
-        System.out.println(str);
-        System.out.println(strNext);
-        System.out.println(str.(strNext));
+        answer = new long [numbers.length];
+        StringBuilder sb = new StringBuilder();
+        long x = 0;
+        for(int i = 0; i < numbers.length; i++) {
+        	sb.insert(0, (Long.toBinaryString(numbers[i])));
+        	x = sb.lastIndexOf("0");
+        	if(x < 0) {
+        		sb.replace(0, 1, "10");
+        	} else if(x == sb.length() - 1) {
+        		sb.setCharAt((int) x, '1');
+        	} else {
+        		sb.setCharAt((int) x + 1, '0');
+        		sb.setCharAt((int) x, '1');
+        	}
+        	answer[i] = Long.parseLong(sb.toString(), 2);
+        	sb.delete(0, sb.length());
+        }
+        return answer;
         
+        
+//        long[] answer = {};
+//        String str = "";
+//        String strNext = "";
+//        answer = new long[numbers.length];
+//        long lNum = 0;
+//        int cnt = 0,x = 0, y = 0;;
+//        
 //        for(int i = 0; i < numbers.length; i++) {
-//        	str = Integer.toBinaryString((int) numbers[i]);
-//        	x = (int) numbers[i];
-//        	for(int j = x + 1; j < 500 ;j++) {
-//        		strNext = Integer.toBinaryString(j);
-//        		System.out.println(x);
-//        		System.out.println(strNext);
-//        		System.out.println("==================================");
-//				/*
-//				 * if(str.compareTo(strNext) <= 3) { answer[i] = x; }
-//				 */
+//        	str = Long.toBinaryString(numbers[i]);
+//        	lNum = numbers[i] + 1;
+//        	for(;; lNum++) {
+//        		x = str.length() - 1;
+//        		cnt = 0;
+//        		strNext = Long.toBinaryString(lNum);
+//        		y = strNext.length() - 1;
+//        		while(x >= 0 && cnt < 3) {
+//        			if(str.charAt(x) != strNext.charAt(y)) {
+//        				cnt++;
+//        			}
+//        			x--;
+//        			y--;
+//        		}
+//        		if(x == -1) {
+//        			cnt = cnt + y + 1;
+//        		}
+//        		if(cnt > 0 && cnt <= 2) {
+//        			answer[i] = lNum;
+//        			break;
+//        		}
 //        	}
 //        }
-        return answer;
+//        return answer;
 	}
 }
