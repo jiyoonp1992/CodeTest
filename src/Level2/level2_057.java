f9289864d65e9cae229f4148a71436c834281cef
jiyoonpark <jiyoonp1992@gmail.com>
2023-06-20 17:41:59 +0900

    0620

--- src/Level2/level2_057.java
+++ src/Level2/level2_057.java
@@ -9,19 +9,40 @@ import java.util.stream.Collectors;
 public class level2_057 {
 	//큰 수 만들기
 	public static void main(String[] args) {
-		System.out.println(solution("654321", 5));
+		System.out.println(solution("54321", 2));
 		//System.out.println(Arrays.toString(solution()));
 	}
 	
 	public static String solution(String number, int k) {
 		String answer = "";
+		String[] strArr = {};
 		StringBuffer sb = new StringBuffer();
+		strArr = number.substring(0,k + 1).split("");
+		Arrays.sort(strArr);
+		if(strArr.length != 1) {
+			answer = number.substring(number.indexOf(strArr[strArr.length - 1]), number.indexOf(strArr[strArr.length - 1]) + 1);
+			sb.append(number.substring(number.indexOf(strArr[strArr.length - 1]), number.indexOf(strArr[strArr.length - 1]) + 1));
+			k = k - number.indexOf(strArr[strArr.length - 1]); 
+			sb.append(number.substring(number.indexOf(strArr[strArr.length - 1]) + 1));
+		} else {
+			sb.append(number);
+		}
 		int sumCnt = 0;
-		sb.append(number);
-		boolean st = true;
+		boolean st = answer.length() == (sb.length() - k) ? false : true;
 		while(st) {
-			for(int i = 0; i < sb.length();i++) {
-				System.out.println(sb);
+			System.out.println(sb);
+			if(k == 0 || sb.length() == k) {
+				answer = sb.toString();
+				st = false;
+				break;
+			}
+			if(sumCnt == number.length()) {
+				answer = sb.substring(0, sb.length()-k);
+				st = false;
+				break;
+			}
+			
+			for(int i = 0; i < sb.length(); i++) {
 				if(i < sb.length() - 1) {
 					if(sb.charAt(i) < sb.charAt(i + 1)) {
 						sb.deleteCharAt(i);
@@ -45,18 +66,48 @@ public class level2_057 {
 						sumCnt++;
 					}
 				}
-				if(k == 0) {
-					answer = sb.toString();
-					st = false;
-					break;
-				}
-				if(sumCnt == number.length()) {
-					answer = sb.substring(0, sb.length()-k);
-					st = false;
-					break;
-				}
 			}
 		}
+		
+//		StringBuffer sb = new StringBuffer();
+//		boolean st = sb.length() == (number.length() - k) ? false : true;
+//		while(st) {
+//			for(int i = 0; i < number.length(); i++) {
+//				if(i < number.length() - 1) {
+//					if(sb.charAt(i) < sb.charAt(i + 1)) {
+//						sb.deleteCharAt(i);
+//						k--;
+//						i--;
+//						sumCnt = 0;
+//					} else if(sb.charAt(i) > sb.charAt(i + 1)) {
+//						if(i < sb.length() - 2) {
+//							if(sb.charAt(i + 1) < sb.charAt(i + 2)) {
+//								sumCnt = 0;
+//								sb.deleteCharAt(i + 1);
+//								k--;
+//								i--;
+//							}
+//						} else if(k == 1 && sb.charAt(i) > sb.charAt(i + 1)) {
+//							sumCnt = 0;
+//							sb.deleteCharAt(i + 1);
+//							k--;
+//						}
+//					} else {
+//						sumCnt++;
+//					}
+//				}
+//				if(k == 0) {
+//					answer = sb.toString();
+//					st = false;
+//					break;
+//				}
+//				if(sumCnt == number.length()) {
+//					answer = sb.substring(0, sb.length()-k);
+//					st = false;
+//					break;
+//				}
+//			}
+//		}
 		
 //        String answer = "";
 //        List<Integer> list = new LinkedList<>();
