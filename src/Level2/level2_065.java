30da15effcba648860c953cbfec6787c41a38c95
jiyoonpark <jiyoonp1992@gmail.com>
2023-07-17 17:50:29 +0900

    0717

--- src/Level2/level2_065.java
+++ src/Level2/level2_065.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class level2_065 {
 	//[3차] 방금그곡
 	public static void main(String[] args) {
-		String[] strArr = {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"};
-		String str = "CC#BCC#BCC#BCC#B";
+		String[] strArr = {"13:00,13:05,WORLD,ABCDEF"};
+		String str = "ABC";
 		System.out.println(solution(str,strArr));
 		//System.out.println(Arrays.toString(solution()));
 	}
@@ -17,27 +17,50 @@ public class level2_065 {
         StringBuilder sb = new StringBuilder();
         String str = "";
         int minute = 0, max = 0;
+        int bfHH = 0, bfMM = 0, afHH = 0, afMM = 0;
         for(int i = 0; i < musicinfos.length; i++) {
         	strArr = musicinfos[i].split(",");
-        	minute = Integer.parseInt(strArr[1].replace(":", "")) - Integer.parseInt(strArr[0].replace(":", ""));
+        	afHH = Integer.parseInt(strArr[0].substring(0,2));
+        	afMM = Integer.parseInt(strArr[0].substring(3,5));
+        	bfHH = Integer.parseInt(strArr[1].substring(0,2));
+        	bfMM = Integer.parseInt(strArr[1].substring(3,5));
+        	if(((afHH * 60) + afMM) > ((bfHH * 60) + bfMM)) {
+        		bfHH = 24;
+        	}
         	
+        	minute = ((bfHH * 60) + bfMM) - ((afHH * 60) + afMM);
+        	
+        	strArr[3] = strArr[3].toLowerCase();
+        	m = m.toLowerCase();
+        	while(strArr[3].contains("#")) {
+        		int idx = strArr[3].indexOf("#");
+        		String reStr = strArr[3].substring(idx-1, idx);
+        		strArr[3] = strArr[3].replace(reStr + "#", reStr.toUpperCase());
+        	}
+        	
+        	while(m.contains("#")) {
+        		int idx = m.indexOf("#");
+        		String reStr = m.substring(idx-1, idx);
+        		m = m.replace(reStr + "#", reStr.toUpperCase());
+        	}
         	sb.append(strArr[3]);
         	while(minute > sb.length()) {
         		sb.append(strArr[3]);
         	}
-        	
-        	if(sb.length() > minute) {
-        		sb.substring(0, minute);
-        	}
         	str = sb.toString();
+        	if(str.length() > minute) {
+        		str = str.substring(0, minute);
+        	}
+        	System.out.println(str);
         	
-        	str = str.toLowerCase().replace("a#", "A").replace("c#", "C").replace("d#", "D").replace("f#", "F").replace("g#", "G");
-        	m = m.toLowerCase().replace("a#", "A").replace("c#", "C").replace("d#", "D").replace("f#", "F").replace("g#", "G");
+        	if(m.length() > minute) {
+        		m = m.substring(0, minute);
+        	}
         	
-        	if(str.contains(m) && minute >= max) {
+        	if(str.contains(m) && minute > max) {
         		answer = strArr[2];
         		max = minute;
-        	} else if(answer.equals("(None)") || answer.equals("")) {
+        	} else if(answer.equals("(None)") || answer.isEmpty()) {
         		answer = "(None)";
         	}
         	sb.delete(0, sb.length());
